c7db048a5dc9a056348aaef8f07d4026fe1446b4
Ephraim Muhia <emuhia@ona.io>
2017-07-25 15:11:45 +0300

    Removed native json library
    Signed-off-by: Ephraim Muhia <emuhia@ona.io>

--- android-json-form-wizard/src/main/java/com/vijay/jsonwizard/mvp/MvpPresenter.java
+++
@@ -1,8 +0,0 @@
-package com.vijay.jsonwizard.mvp;
-
-public interface MvpPresenter<V extends MvpView> {
-
-    public void attachView(V view);
-
-    public void detachView(boolean retainInstance);
-}
--- android-json-form-wizard/src/main/java/com/vijay/jsonwizard/mvp/MvpView.java
+++
@@ -1,4 +0,0 @@
-package com.vijay.jsonwizard.mvp;
-
-public interface MvpView {
-}
