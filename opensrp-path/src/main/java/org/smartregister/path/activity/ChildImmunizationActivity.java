74b0af73d4ac60c38acc1a1d224d3f5c5511424d
Martin Ndegwa <ndegwamartin@gmail.com>
2017-09-15 17:15:41 +0300

    Fix bug in weight modal
    Signed-off-by: Martin Ndegwa <ndegwamartin@gmail.com>

--- opensrp-path/src/main/java/org/smartregister/path/activity/ChildImmunizationActivity.java
+++ opensrp-path/src/main/java/org/smartregister/path/activity/ChildImmunizationActivity.java
@@ -578,6 +578,13 @@ public class ChildImmunizationActivity extends BaseActivity
     private void updateRecordWeightViews(WeightWrapper weightWrapper) {
         View recordWeight = findViewById(R.id.record_weight);
         recordWeight.setClickable(true);
+        recordWeight.setBackground(getResources().getDrawable(R.drawable.record_weight_bg));
+
+        TextView recordWeightText = (TextView) findViewById(R.id.record_weight_text);
+        recordWeightText.setText(R.string.record_weight);
+
+        ImageView recordWeightCheck = (ImageView) findViewById(R.id.record_weight_check);
+        recordWeightCheck.setVisibility(View.GONE);
         recordWeight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -585,23 +592,24 @@ public class ChildImmunizationActivity extends BaseActivity
             }
         });
 
-        recordWeight.setBackground(getResources().getDrawable(R.drawable.record_weight_bg));
         if (weightWrapper.getDbKey() != null && weightWrapper.getWeight() != null) {
-            TextView recordWeightText = (TextView) findViewById(R.id.record_weight_text);
             recordWeightText.setText(Utils.kgStringSuffix(weightWrapper.getWeight()));
-
-            ImageView recordWeightCheck = (ImageView) findViewById(R.id.record_weight_check);
             recordWeightCheck.setVisibility(View.VISIBLE);
 
             if (weightWrapper.getUpdatedWeightDate() != null) {
-                long timeDiff = Calendar.getInstance().getTimeInMillis()
-                        - weightWrapper.getUpdatedWeightDate().getMillis();
+                long timeDiff = Calendar.getInstance().getTimeInMillis() - weightWrapper.getUpdatedWeightDate().getMillis();
 
-                if (timeDiff < TimeUnit.MILLISECONDS.convert(1, TimeUnit.DAYS)) {
+                if (timeDiff <= TimeUnit.MILLISECONDS.convert(12, TimeUnit.HOURS)) {
                     //disable the button
                     recordWeight.setClickable(false);
                     recordWeight.setBackground(new ColorDrawable(getResources()
                             .getColor(android.R.color.transparent)));
+                } else {
+                    //reset state
+                    recordWeight.setClickable(true);
+                    recordWeight.setBackground(getResources().getDrawable(R.drawable.record_weight_bg));
+                    recordWeightText.setText(R.string.record_weight);
+                    recordWeightCheck.setVisibility(View.GONE);
                 }
             }
         }
