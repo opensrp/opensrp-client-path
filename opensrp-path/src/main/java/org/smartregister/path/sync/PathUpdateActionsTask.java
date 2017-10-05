54502b8c3af00c9b18260fd34bc7b06e10dec3ca
Ephraim Kigamba <ekigamba@ona.io>
2017-10-05 12:24:54 +0300

    Fix error

--- opensrp-path/src/main/java/org/smartregister/path/sync/PathUpdateActionsTask.java
+++ opensrp-path/src/main/java/org/smartregister/path/sync/PathUpdateActionsTask.java
@@ -118,7 +118,7 @@ public class PathUpdateActionsTask {
                         }
                     }
 
-                    return (fetchStatusForForms == fetched) ? fetchStatusForActions : return fetchStatusForForms;
+                    return (fetchStatusForForms == fetched) ? fetchStatusForActions : fetchStatusForForms;
 
                 }
 
