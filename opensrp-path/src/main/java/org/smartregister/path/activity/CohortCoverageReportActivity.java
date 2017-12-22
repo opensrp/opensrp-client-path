3b2a503bd0466a40fa3c8827bbe65abad2d4b1eb
Ephraim Muhia <emuhia@ona.io>
2017-12-22 10:14:18 +0300

    Cohort coverage report UI
    Signed-off-by: Ephraim Muhia <emuhia@ona.io>

--- opensrp-path/src/main/java/org/smartregister/path/activity/CohortCoverageReportActivity.java
+++ opensrp-path/src/main/java/org/smartregister/path/activity/CohortCoverageReportActivity.java
@@ -1,21 +1,42 @@
 package org.smartregister.path.activity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.smartregister.domain.FetchStatus;
+import org.smartregister.immunization.db.VaccineRepo;
 import org.smartregister.path.R;
+import org.smartregister.path.helper.SpinnerHelper;
 import org.smartregister.path.toolbar.LocationSwitcherToolbar;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+import util.PathConstants;
 
 /**
  * Created by keyman on 21/12/17.
  */
 public class CohortCoverageReportActivity extends BaseActivity {
+
+    private static final SimpleDateFormat MMMYYYY = new SimpleDateFormat("MMMM yyyy");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +61,7 @@ public class CohortCoverageReportActivity extends BaseActivity {
 
         ((TextView) toolbar.findViewById(R.id.title)).setText(getString(R.string.cohort_coverage_report));
 
+        updateListViewHeader();
     }
 
     @Override
@@ -53,6 +75,21 @@ public class CohortCoverageReportActivity extends BaseActivity {
         final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         LinearLayout hia2 = (LinearLayout) drawer.findViewById(R.id.coverage_reports);
         hia2.setBackgroundColor(getResources().getColor(R.color.tintcolor));
+
+        updateReportList();
+
+        List<Date> dates = new ArrayList<>();
+
+        Calendar c = Calendar.getInstance();
+        dates.add(c.getTime());
+
+        for (int i = 0; i < 5; i++) {
+            c.add(Calendar.MONTH, -1);
+            dates.add(c.getTime());
+
+        }
+
+        updateReportDates(dates);
     }
 
     @Override
@@ -80,4 +117,149 @@ public class CohortCoverageReportActivity extends BaseActivity {
         return null;
     }
 
+    private void updateListViewHeader() {
+        // Add header
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        View view = getLayoutInflater().inflate(R.layout.cohort_coverage_report_header, null);
+        listView.addHeaderView(view);
+    }
+
+    private void updateReportList() {
+        final List<VaccineRepo.Vaccine> vaccineList = VaccineRepo.getVaccines(PathConstants.EntityType.CHILD);
+
+
+        if (vaccineList == null || vaccineList.isEmpty()) {
+            return;
+        }
+
+        BaseAdapter baseAdapter = new BaseAdapter() {
+            @Override
+            public int getCount() {
+                return vaccineList.size();
+            }
+
+            @Override
+            public Object getItem(int position) {
+                return vaccineList.get(position);
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return position;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view;
+                LayoutInflater inflater =
+                        CohortCoverageReportActivity.this.getLayoutInflater();
+                if (convertView == null) {
+                    view = inflater.inflate(R.layout.cohort_coverage_report_item, null);
+                } else {
+                    view = convertView;
+                }
+
+                VaccineRepo.Vaccine vaccine = vaccineList.get(position);
+
+                TextView vaccineTextView = (TextView) view.findViewById(R.id.vaccine);
+                vaccineTextView.setText(vaccine.display());
+
+                Random r = new Random();
+                int Low = 0;
+                int High = 100;
+                int result = r.nextInt(High - Low) + Low;
+
+                TextView vaccinatedTextView = (TextView) view.findViewById(R.id.vaccinated);
+                vaccinatedTextView.setText(String.valueOf(result));
+
+                TextView coverageTextView = (TextView) view.findViewById(R.id.coverage);
+                coverageTextView.setText(String.format(getString(R.string.coverage_percentage),
+                        result));
+
+                return view;
+            }
+        };
+
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(baseAdapter);
+    }
+
+    private void updateReportDates(List<Date> dates) {
+        if (dates != null && !dates.isEmpty()) {
+            View reportDateSpinnerView = findViewById(R.id.cohort_spinner);
+            if (reportDateSpinnerView != null) {
+                SpinnerHelper reportDateSpinner = new SpinnerHelper(reportDateSpinnerView);
+                SpinnerAdapter dataAdapter = new SpinnerAdapter(this, R.layout.item_spinner, dates);
+                dataAdapter.setDropDownViewResource(R.layout.item_spinner_drop_down);
+                reportDateSpinner.setAdapter(dataAdapter);
+
+                reportDateSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        Object tag = view.getTag();
+                        if (tag != null && tag instanceof Date) {
+                            updateReportList();
+                        }
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+
+                    }
+                });
+            }
+        }
+    }
+
+
+    ////////////////////////////////////////////////////////////////
+    // Inner classes
+    ////////////////////////////////////////////////////////////////
+
+    private class SpinnerAdapter extends ArrayAdapter<Date> {
+
+        SpinnerAdapter(Context context, int resource, List<Date> objects) {
+            super(context, resource, objects);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view;
+            if (convertView == null) {
+                view = getLayoutInflater().inflate(R.layout.item_spinner, parent, false);
+            } else {
+                view = convertView;
+            }
+
+            if (view instanceof TextView) {
+                TextView textView = (TextView) view;
+                Date date = getItem(position);
+
+                String dateString = MMMYYYY.format(date);
+                textView.setText(dateString);
+                textView.setTag(date);
+            }
+            return view;
+        }
+
+        @Override
+        public View getDropDownView(int position, View convertView, ViewGroup parent) {
+            View view;
+            if (convertView == null) {
+                view = getLayoutInflater().inflate(R.layout.item_spinner_drop_down, parent, false);
+            } else {
+                view = convertView;
+            }
+
+            if (view instanceof TextView) {
+                TextView textView = (TextView) view;
+                Date date = getItem(position);
+
+                String dateString = MMMYYYY.format(date);
+                textView.setText(dateString);
+                textView.setTag(date);
+            }
+            return view;
+        }
+    }
 }
